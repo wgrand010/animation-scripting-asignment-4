e95c147bfe1d3617b13f7f4e9bfd07237da45392
wgrand <wgrand010@gmail.com>
2020-10-11 20:58:15 -0500

    six

--- smartsave.py
+++ smartsave.py
@@ -108,7 +108,30 @@ class SmartSaveUI(QtWidgets.QDialog):
         layout.addWidget(self.ext_lbl, 1, 5)
         return layout
 
-    
+    def _create_filename_headers(self):
+        self.descriptor_header_lbl = QtWidgets.QLabel("descriptor")
+        self.descriptor_header_lbl.setStyleSheet("font: bold")
+        self.task_header_lbl = QtWidgets.QLabel("Task")
+        self.task_header_lbl.setStyleSheet("font: bold")
+        self.ver_header_lbl = QtWidgets.QLabel("Version")
+        self.ver_header_lbl.setStyleSheet("font: bold")
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.descriptor_header_lbl, 0, 0)
+        layout.addWidget(self.task_header_lbl, 0, 2)
+        layout.addWidget(self.ver_header_lbl, 0, 4)
+        return layout
+
+    def _create_folder_ui(self):
+        default_folder = Path(cmds.workspace(rootDirectory=True, query=True))
+        default_folder = default_folder / "scenes"
+        self.folder_le = QtWidgets.QLineEdit(default_folder)
+        self.folder_browse_btn = QtWidgets.QPushButton("...")
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.folder_le)
+        layout.addWidget(self.folder_browse_btn)
+        return layout
+
+
 class SceneFile(object):
     def __init__(self, path=None):
         self._folder_path = Path(cmds.workspace(query=True,
