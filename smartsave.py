a8d9f12b9c1f6ae871d46aea28375070b3c284d8
wgrand <wgrand010@gmail.com>
2020-10-11 20:55:41 -0500

    second

--- smartsave.py
+++ smartsave.py
@@ -1,6 +1,52 @@
 import logging
 
 
+from PySide2 import QtWidgets, QtCore
+from shiboken2 import wrapInstance
+import maya.OpenMayaUI as omui
+import maya.cmds as cmds
+import pymel.core as pmc
+from pymel.core.system import Path
+
+log = logging.getLogger(__name__)
+
+
+
+def maya_main_window():
+    main_window = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window), QtWidgets.QWidget)
+
+
+class SmartSaveUI(QtWidgets.QDialog):
+    """Smart Save UI Class"""
+
+    def __init__(self):
+        super(SmartSaveUI, self).__init__(parent=maya_main_window())
+        self.setWindowTitle("Smart Save")
+        self.setMinimumWidth(500)
+        self.setMaximumHeight(200)
+        self.setWindowFlags(self.windowFlags() ^
+                            QtCore.Qt.WindowContextHelpButtonHint)
+        self.scenefile = SceneFile()
+        self.create_ui()
+        self.create_connections()
+
+
+    def create_ui(self):
+        self.title_lbl = QtWidgets.QLabel("Smart Save")
+        self.title_lbl.setStyleSheet("font: bold 20px")
+        self.folder_lay = self._create_folder_ui()
+        self.filename_lay = self._create_filename_ui()
+        self.button_lay = self._create_button_ui()
+        self.main_lay = QtWidgets.QVBoxLayout()
+        self.main_lay.addWidget(self.title_lbl)
+        self.main_lay.addLayout(self.folder_lay)
+        self.main_lay.addLayout(self.filename_lay)
+        self.main_lay.addStretch()
+        self.main_lay.addLayout(self.button_lay)
+        self.setLayout(self.main_lay)
+
+    d
 class SceneFile(object):
     def __init__(self, path=None):
         self._folder_path = Path(cmds.workspace(query=True,
